d3fdb9be6f8bae980a43236a887f0b1435333163
Fadell-Karlsefni <fadilah3109@gmail.com>
2025-10-21 14:09:45 +0800

    Menambahkan Fitur Tambah Tugas di app.js nya

---
+++ app.js
@@ -0,0 +1,29 @@
+const tasks = localStorage.getItem("tasks") ? JSON.parse(localStorage.getItem("tasks")) :
+ [] ;
+
+const tasksList = document.getElementById("tasks-list");
+ const taskForm = document.getElementById("taskForm");
+
+ taskForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    addTask();
+ })
+
+ function addTask() {
+    const taskTitleValue = document.getElementById("taskTitle").value;
+
+    if (taskTitleValue.trim() == "") {
+        alert("Judul Tugas Tidak Boleh Kosong");
+        return;
+    }
+
+    const newTask = {
+        id: tasks.length + 1,
+        title: taskTitleValue.trim(),
+
+    };
+
+    tasks.push(newTask);
+    document.getElementById("taskTitle").value = "";
+    localStorage.setItem("tasks",JSON.stringify(tasks));
+ }
