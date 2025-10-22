009c80df0d3061de3e2a9e0d4c38ff34604d0d54
Fadell-Karlsefni <fadilah3109@gmail.com>
2025-10-22 11:22:36 +0800

    Menambahkan function baru yaitu delete dan checkTask, fungsi nya untuk menghapus daftar tugas yang selesai dan memberikan warna element kalau sudah di checklist tugas yang selesai

--- app.js
+++ app.js
@@ -67,4 +67,26 @@ const tasksList = document.getElementById("tasks-list");
     tasksList.appendChild(li);
   });
  }
+
+function deleteTask(index) {
+    tasks.splice(index, 1);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    renderTask();
+}
+
+function checkTask(index) {
+    const taskElement = document.getElementById(`task${index}`);
+    const checkboxElement = document.getElementById(`checkTask${index}`);
+
+    if (checkboxElement.checked) {
+        taskElement.classList.add("checked");
+        tasks[index].checked =  true;
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+    } else {
+        taskElement.classList.remove("checked");
+        tasks[index].checked = false ;
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+    }
+}
+
  renderTask();
