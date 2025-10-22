b8d071a0b8c7e4983d24622152fba753f00a1095
Fadell-Karlsefni <fadilah3109@gmail.com>
2025-10-22 11:02:57 +0800

    Membuat function renderTask untuk menampilkan daftar tugas nya

--- app.js
+++ app.js
@@ -26,4 +26,45 @@ const tasksList = document.getElementById("tasks-list");
     tasks.push(newTask);
     document.getElementById("taskTitle").value = "";
     localStorage.setItem("tasks",JSON.stringify(tasks));
+
+    renderTask()
  }
+
+ function renderTask() {
+  tasksList.innerHTML = ""; 
+
+  if (tasks.length == 0) {
+    const li = document.createElement("li");
+        li.classList.add("tasks-list-item");
+            li.innerHTML = `
+                <div class="tasks">
+                    <div class="tasks-list">
+                        Tidak Ada Kegiatan
+                    </div>
+                </div>
+            `;
+            tasksList.appendChild(li);
+            return;
+        }
+
+  tasks.forEach((task, index) => {
+    const li = document.createElement("li");
+    li.classList.add("tasks-list-item");
+
+    if (task.checked) {
+      li.classList.add("checked");
+    }
+
+    li.innerHTML = `
+      <div class="task">
+        <input type="checkbox" onchange="checkTask(${index})" id="checkTask${index}" ${task.checked ? "checked" : ""}>
+        <span class="task-title">${task.title}</span>
+        <button class="button" onclick="deleteTask(${index})">Hapus</button>
+      </div>
+    `;
+
+    li.id = `task${index}`;
+    tasksList.appendChild(li);
+  });
+ }
+ renderTask();
