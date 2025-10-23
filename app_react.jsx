cee56dd91eb12acb23af802ce7e5b32ca652203e
Fadell-Karlsefni <fadilah3109@gmail.com>
2025-10-23 15:24:32 +0800

    Membuat Fungsi Tambah Tugas di React js dan menyimpan nya di local storage

--- app_react.jsx
+++ app_react.jsx
@@ -1,8 +1,62 @@
-const App = () => {
+const MyTasksForm = (props) => {
+return (
+    <form className="form" id="taskForm" onSubmit={(event) => props.onSubmit(event)}>
+            <div className="form-group">
+                <label htmlFor="taskTitle" className="form-label">
+                    Mau Ngapain Hari Ini?
+                </label>
+                <input className="form-input form-input--full" type="text" id="taskTitle"
+                name="taskTitle" placeholder="Masukkan Kegiatan" onChange={(event) => props.setTaskTitle(event.target.value)}
+                value={props.taskTitle}/>
+            </div>
+            <div className="form-group form-group--button-right">
+                <button className="button" type="submit">Simpan</button>
+            </div>
+        </form>
+)
+};
+
+const MyTasksList = () => {
     return (
-        <div>
-            <h1>Belajar React Di course Wpu</h1>
+        <div className="tasks">
+            <h2 className="tasks-title">Agenda Ku</h2>
+            <ul className="tasks-list" id="tasks-list"></ul>
         </div>
+    )
+};
+
+const MyTasksContainer = (props) => {
+    return <div className="container">{props.children}</div>;
+    
+};
+
+const App = () => {
+
+    const [taskTitle, setTaskTitle] = React.useState("");
+    const [tasks, setTasks] = React.useState([]);
+
+    const handleAddTasks = (event) => {
+        event.preventDefault();
+        if (taskTitle.trim() === "") {
+            alert("Judul Tugas Tidak Boleh Kosong");
+            return;
+        }
+
+    const newTask = {
+        id: tasks.length + 1,
+        title: taskTitle,
+    };
+
+    setTasks([...tasks, newTask]);
+    setTaskTitle("");
+    localStorage.setItem("tasks", JSON.stringify([...tasks, newTask]))
+    }
+
+    return (
+        <MyTasksContainer>
+            <MyTasksForm onSubmit={handleAddTasks} taskTitle={taskTitle} setTaskTitle={setTaskTitle} />
+            <MyTasksList />
+        </MyTasksContainer>
     )
 };  
 
